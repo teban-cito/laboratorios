7bcb794140f0b1d47d45c69b4eb86aa41c397be6
teban-cito <esteban.vargas.ttv.181@unilibre.edu.co>
2024-11-29 16:39:25 -0500

    Actualizando parte 4 repositorio de laboratorios en GitHub

--- LABORATORIO_5/aplicacion.php
+++ LABORATORIO_5/aplicacion.php
@@ -35,10 +35,9 @@
             $nombre = htmlspecialchars($_POST['nombre']);
             $email = htmlspecialchars($_POST['email']);
             $telefono = htmlspecialchars($_POST['telefono']);
-            $password = htmlspecialchars($_POST['password']);
+            $cedula = htmlspecialchars($_POST['cedula']);
             $fecha_nacimiento = htmlspecialchars($_POST['fecha_nacimiento']);
-            $pais = htmlspecialchars($_POST['pais']);
-            $mensaje = htmlspecialchars($_POST['mensaje']);
+            $carrera = htmlspecialchars($_POST['carrera']);
 
             // Muestra los datos recibidos
             echo "<h1>Datos recibidos del formulario:</h1>";
@@ -46,10 +45,9 @@
             echo "<p><strong>Nombre:</strong> $nombre</p>";
             echo "<p><strong>Correo Electrónico:</strong> $email</p>";
             echo "<p><strong>Teléfono:</strong> $telefono</p>";
-            echo "<p><strong>Contraseña:</strong> (Oculta por seguridad)</p>";
+            echo "<p><strong>Cedula:</strong> $cedula</p>";
             echo "<p><strong>Fecha de Nacimiento:</strong> $fecha_nacimiento</p>";
-            echo "<p><strong>País:</strong> $pais</p>";
-            echo "<p><strong>Mensaje:</strong> $mensaje</p>";
+            echo "<p><strong>Carrera:</strong> $carrera</p>";
         } else {
             // Si se accede directamente al archivo sin enviar el formulario
             echo "<h1>Acceso no autorizado</h1>";
